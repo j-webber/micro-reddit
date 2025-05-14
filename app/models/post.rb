cb28c8946e93daddef5e469e571e467a31e4aee2
Joe Webber <35155442+j-webber@users.noreply.github.com>
2025-05-14 11:45:28 -0400

    added comments table, relationships, and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :link_url, presence: true, format: { with: URI.regexp(%w[http https]), message: "must be a valid URL: http://example.com" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :email_address, :username, presence: true, uniqueness: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
