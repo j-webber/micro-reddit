cb644a69bea7fc47274f6842316972c4869dd5d6
Joe Webber <35155442+j-webber@users.noreply.github.com>
2025-05-14 11:45:51 -0400

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_14_151821) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_14_153428) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "link_url"
@@ -40,6 +50,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_14_151821) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "sessions", "users"
 end
